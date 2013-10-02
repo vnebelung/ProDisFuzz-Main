7ad25a7627a0afbebdb790be9344725091fb8ad9
Volker Nebelung <vnebelung@prodisfuzz.net>
2013-10-02 08:12:21 +0200

    Release "Green River"

---
+++ src/model/logger/logger.java
@@ -0,0 +1,132 @@
+/*
+ * This file is part of ProDisFuzz, modified on 01.10.13 23:25.
+ * Copyright (c) 2013 Volker Nebelung <vnebelung@prodisfuzz.net>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+package model.logger;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observable;
+
+public final class Logger extends Observable {
+
+    public static final int MAX_ENTRIES = 500;
+    private static final Logger INSTANCE = new Logger();
+    private final List<Message> log;
+
+    /**
+     * Singelton Constructor.
+     */
+    private Logger() {
+        super();
+        log = new LinkedList<>();
+    }
+
+    /**
+     * Gets the only instance of Logger.
+     */
+    public static Logger getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Adds an info message to the log.
+     *
+     * @param message the message to be add
+     */
+    public void info(final String message) {
+        log.add(new Message(message, Message.Type.INFO));
+        prune();
+        spreadUpdate();
+    }
+
+    /**
+     * Adds a success message to the log.
+     *
+     * @param message the message to be add
+     */
+    public void fine(final String message) {
+        log.add(new Message(message, Message.Type.FINE));
+        prune();
+        spreadUpdate();
+    }
+
+    /**
+     * Adds an error message to the log.
+     *
+     * @param message the message to be add
+     */
+    public void error(final String message) {
+        log.add(new Message(message, Message.Type.ERROR));
+        prune();
+        spreadUpdate();
+    }
+
+    /**
+     * Adds an error message based on an occured exception to the log.
+     *
+     * @param t the throwable
+     */
+    public void error(final Throwable t) {
+        try (StringWriter sw = new StringWriter()) {
+            try (PrintWriter pw = new PrintWriter(sw)) {
+                t.printStackTrace(pw);
+                error(sw.toString());
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    /**
+     * Adds a warning message to the log.
+     *
+     * @param message the message to be add
+     */
+    public void warning(final String message) {
+        log.add(new Message(message, Message.Type.WARNING));
+        prune();
+        spreadUpdate();
+    }
+
+    /**
+     * Notifies all observers about an update.
+     */
+    private void spreadUpdate() {
+        setChanged();
+        notifyObservers();
+    }
+
+    /**
+     * Keeps the number of entries of the log under the defined size.
+     */
+    private void prune() {
+        final int oversize = log.size() - MAX_ENTRIES;
+        if (oversize > 0) {
+            log.subList(0, oversize).clear();
+        }
+    }
+
+    /**
+     * Gets the last log entry.
+     *
+     * @return the last log entry or null if there are no entries
+     */
+    public Message getLastEntry() {
+        return log.isEmpty() ? null : log.get(log.size() - 1);
+    }
+
+    /**
+     * Resets the log and deletes all entries.
+     */
+    public void reset() {
+        log.clear();
+        spreadUpdate();
+    }
+}
---
+++ src/xml/XMLNames.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of ProDisFuzz, modified on 01.10.13 23:25.
+ * Copyright (c) 2013 Volker Nebelung <vnebelung@prodisfuzz.net>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+package xml;
+
+public class XmlNames {
+
+    public final static String ROOT = "prodisfuzz";
+    public final static String PARTS = "protocolparts";
+    public final static String PART_VAR = "partvar";
+    public final static String PART_FIX = "partfix";
+    public final static String CONTENT = "content";
+    public final static String BYTE = "byte";
+    public final static String MAXLENGTH = "maxlength";
+    public final static String MINLENGTH = "minlength";
+}
